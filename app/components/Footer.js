611f449cd4dd786d4aeb071a30d8dbcc87c061a5
Patrick <pfkellogg@gmail.com>
2026-01-02 11:58:05 -0500

    routing (spa) - 20

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,38 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <footer className='border-top text-center small text-muted py-3'>
       <p>
-        <a
-          href='/'
+        <Link
+          to='/'
           className='mx-1'
         >
           Home
-        </a>
+        </Link>
         |
-        <a
+        <Link
           className='mx-1'
-          href='/about-us'
+          to='/about-us'
         >
           About Us
-        </a>
+        </Link>
         |
-        <a
+        <Link
           className='mx-1'
-          href='/terms'
+          to='/terms'
         >
           Terms
-        </a>
+        </Link>
       </p>
       <p className='m-0'>
-        Copyright &copy; 2025
-        <a
-          href='/'
+        Copyright &copy; {new Date().getFullYear() + ' '}
+        <Link
+          to='/'
           className='text-muted'
         >
           ComplexApp
-        </a>
+        </Link>
         . All rights reserved.
       </p>
     </footer>
