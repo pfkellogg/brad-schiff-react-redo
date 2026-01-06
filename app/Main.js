e69e7e7eb7608ede9e5f18c8dd53d1a220e7cca7
Patrick <pfkellogg@gmail.com>
2026-01-06 12:59:14 -0500

    profile screen

--- app/Main.js
+++ app/Main.js
@@ -20,6 +20,7 @@ import Home from './components/Home'
 import CreatePost from './components/CreatePost'
 import ViewSinglePost from './components/ViewSinglePost'
 import FlashMessages from './components/FlashMessages'
+import Profile from './components/Profile'
 
 function Main() {
   const initialState = {
@@ -68,6 +69,7 @@ function Main() {
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Routes>
+            <Route path='/profile/:username/*' element={<Profile />} />
             <Route path='/' element={state.loggedIn ? <Home /> : <HomeGuest />} />
             <Route path='/post/:id' element={<ViewSinglePost />} />
             <Route path='/create-post' element={<CreatePost />} />
--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,33 +5,21 @@ function Footer() {
   return (
     <footer className='border-top text-center small text-muted py-3'>
       <p>
-        <Link
-          to='/'
-          className='mx-1'
-        >
+        <Link to='/' className='mx-1'>
           Home
         </Link>
         |
-        <Link
-          className='mx-1'
-          to='/about-us'
-        >
+        <Link className='mx-1' to='/about-us'>
           About Us
         </Link>
         |
-        <Link
-          className='mx-1'
-          to='/terms'
-        >
+        <Link className='mx-1' to='/terms'>
           Terms
         </Link>
       </p>
       <p className='m-0'>
         Copyright &copy; {new Date().getFullYear() + ' '}
-        <Link
-          to='/'
-          className='text-muted'
-        >
+        <Link to='/' className='text-muted'>
           ComplexApp
         </Link>
         . All rights reserved.
