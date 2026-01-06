c0b36ff8c22541cd17286cddf7abab7358f97e7c
Patrick <pfkellogg@gmail.com>
2026-01-06 15:21:50 -0500

    load posts by author

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -2,8 +2,8 @@ import React, { useEffect, useContext, useState } from 'react'
 import Page from './Page'
 import { useParams } from 'react-router-dom'
 import Axios from 'axios'
-import SateContext from '../StateContext'
 import StateContext from '../StateContext'
+import ProfilePosts from './ProfilePosts'
 
 function Profile() {
   const { username } = useParams()
@@ -49,20 +49,7 @@ function Profile() {
         </a>
       </div>
 
-      <div className='list-group'>
-        <a href='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #1</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </a>
-        <a href='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #2</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </a>
-        <a href='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #3</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </a>
-      </div>
+      <ProfilePosts />
     </Page>
   )
 }
