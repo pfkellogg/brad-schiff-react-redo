96e397c4488b2439d54b05a2a9b984c6fd5e19d1
Patrick <pfkellogg@gmail.com>
2026-01-10 16:35:22 -0500

    redo - setting up our workflow - 15

--- app/Main.js
+++ app/Main.js
@@ -1,108 +1,17 @@
-import React, { useRef, useState, useEffect, useReducer } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { useImmerReducer } from 'use-immer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { CSSTransition } from 'react-transition-group'
-import Axios from 'axios'
-Axios.defaults.baseURL = 'http://localhost:8080/'
 
-import StateContext from './StateContext'
-import DispatchContext from './DispatchContext'
-
-// My Components
-import Header from './components/Header'
-import HomeGuest from './components/HomeGuest'
-import Footer from './components/Footer'
-import About from './components/About'
-import Terms from './components/Terms'
-import Home from './components/Home'
-import CreatePost from './components/CreatePost'
-import ViewSinglePost from './components/ViewSinglePost'
-import FlashMessages from './components/FlashMessages'
-import Profile from './components/Profile'
-import EditPost from './components/EditPost'
-import NotFound from './components/NotFound'
-import Search from './components/Search'
-
-function Main() {
-  const searchRef = useRef(null)
-
-  const initialState = {
-    loggedIn: Boolean(localStorage.getItem('complexappToken')),
-    flashMessages: [],
-    user: {
-      token: localStorage.getItem('complexappToken'),
-      username: localStorage.getItem('complexappUsername'),
-      avatar: localStorage.getItem('complexappAvatar')
-    },
-    isSearchOpen: false
-  }
-
-  function ourReducer(draft, action) {
-    switch (action.type) {
-      case 'login':
-        draft.loggedIn = true
-        draft.user = action.data
-        return
-      case 'logout':
-        draft.loggedIn = false
-        return
-      case 'flashMessage':
-        draft.flashMessages.push(action.value)
-        return
-      case 'openSearch':
-        draft.isSearchOpen = true
-        return
-      case 'closeSearch':
-        draft.isSearchOpen = false
-        return
-    }
-  }
-
-  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
-
-  useEffect(() => {
-    if (state.loggedIn) {
-      localStorage.setItem('complexappToken', state.user.token)
-      localStorage.setItem('complexappUsername', state.user.username)
-      localStorage.setItem('complexappAvatar', state.user.avatar)
-    } else {
-      localStorage.removeItem('complexappToken')
-      localStorage.removeItem('complexappUsername')
-      localStorage.removeItem('complexappAvatar')
-    }
-  }, [state.loggedIn])
-
+function ExampleComponent() {
   return (
-    <StateContext.Provider value={state}>
-      <DispatchContext.Provider value={dispatch}>
-        <BrowserRouter>
-          <FlashMessages messages={state.flashMessages} />
-          <Header />
-          <Routes>
-            <Route path='/profile/:username/*' element={<Profile />} />
-            <Route path='/' element={state.loggedIn ? <Home /> : <HomeGuest />} />
-            <Route path='/post/:id' element={<ViewSinglePost />} />
-            <Route path='/post/:id/edit' element={<EditPost />} />
-            <Route path='/create-post' element={<CreatePost />} />
-            <Route path='/about-us' element={<About />} />
-            <Route path='/terms' element={<Terms />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-          <CSSTransition nodeRef={searchRef} timeout={330} in={state.isSearchOpen} classNames='search-overlay' unmountOnExit>
-            <div ref={searchRef} className='search-overlay'>
-              <Search />
-            </div>
-          </CSSTransition>
-          <Footer />
-        </BrowserRouter>
-      </DispatchContext.Provider>
-    </StateContext.Provider>
+    <div>
+      <h1>This is our app!</h1>
+      <p>The sky is blue and the grass is green.</p>
+    </div>
   )
 }
 
 const root = ReactDOM.createRoot(document.querySelector('#app'))
-root.render(<Main />)
+root.render(<ExampleComponent />)
 
 if (module.hot) {
   module.hot.accept()
